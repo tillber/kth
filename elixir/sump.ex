d12ef01682885b6f7319bf52cd92400c32deb377
tillber <tillber@kth.se>
2019-03-07 16:12:16 +0100

    first commit

---
+++ elixir/sump.ex
@@ -0,0 +1,20 @@
+# sump.ex
+defmodule Sump do
+  @type tree :: {:node, integer(), tree(), tree()} | nil
+  @spec sum(tree()) :: integer()
+
+  def sum(nil) do 0 end
+  def sum({:node, value, left, right}) do
+    parent = self()
+    spawn(fn -> send(parent, {:result, sum(left)}) end)
+    spawn(fn -> send(parent, {:result, sum(right)}) end)
+
+    receive do
+      {:result, x} ->
+        receive do
+          {:result, y} ->
+            value + x + y
+        end
+      end
+  end
+end
