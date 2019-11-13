6215c7ceb5e5af6d035649444b6294622f1fb2e4
tillber <tillber@kth.se>
2019-11-13 16:31:18 +0100

    added boot-c directory (initial commit)

---
+++ id1206/boot-c/myos.c
@@ -0,0 +1,27 @@
+//myos.c
+#define COLUMNS 80
+#define ROWS 25
+
+char *name = "Martin's OS is running";
+
+typedef struct vga_char{
+    char character;
+    char colors;
+} vga_char;
+
+void main(void){
+    //The VGA terminal mapped memory
+    vga_char *vga = (vga_char*)0xb8000;
+
+    for(int i = 0; i < COLUMNS*ROWS; i++){
+        vga[i].character = ' ';
+        vga[i].colors = 0x0f;
+    } 
+
+    for(int i = 0; name[i] != '\0'; i++){
+        vga[600+i].character = name[i];
+        vga[600+i].colors = 0x0f;
+    }
+
+    return;
+}
