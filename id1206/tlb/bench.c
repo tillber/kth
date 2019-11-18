17dd34a0807639e3a090eae1747952e464b21e25
tillber <tillber@kth.se>
2019-11-18 15:40:02 +0100

    initial commit

---
+++ id1206/tlb/bench.c
@@ -0,0 +1,50 @@
+//bench.c
+#include  <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#define PAGES (16)
+#define REFS (1024*1024)
+#define PAGESIZE 64
+
+int main(int argc, char *argv[]){
+    clock_t c_start, c_stop;
+
+    char *memory = malloc((long)PAGESIZE * PAGES);
+
+    for(int p = 0; p < PAGES; p++){
+        long ref =(long)p * PAGESIZE;
+        /*force the page to be allocated*/
+        memory[ref] += 1;
+    }
+
+    printf("#TLB experiment\n");
+    printf("# page size = %d bytes\n", (PAGESIZE));
+    printf("# max pages = %d\n", (PAGES));
+    printf("# total number of references = %d Mi\n", (REFS/(1024*1024)));
+    printf("# pages\t proc\t sum\n");
+
+    for(int pages = 1; pages <= PAGES; pages++){
+        int loops = REFS/pages;
+        c_start = clock();
+        long sum = 0;
+
+        for(int l = 0; l < loops; l++){
+            for(int p = 0; p < pages; p++){
+                long ref = (long)p * PAGESIZE;
+                sum += memory[ref];
+                //sum++;
+            }
+        }
+
+        c_stop = clock();
+
+        {
+            double proc;
+            proc = ((double)(c_stop - c_start))/CLOCKS_PER_SEC;
+            printf("%d\t %.6f\t %ld\n", pages, proc, sum);
+        }
+    }
+
+    return 0;
+}
---
+++ id1206/tlb/bench.p
@@ -0,0 +1,13 @@
+set terminal png
+set output "tlb.png"
+
+set title "TLB benchmark, 4 KiByte pages, 10 Gi operations"
+
+set key right center
+
+set xlabel "number of pages"
+set ylabel "time in s"
+
+#use log scale if we use doubling of number of pages set logscale x 2
+
+plot "bench.dat" u 1:2 w linespoints title "page refs"
